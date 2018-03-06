1f0ac0f21c5faef26542dad035dc10ac9ec7701c
sai <saikrishnagottipati@gmail.com>
2018-03-06 14:22:53 -0500

    added PolicyValNetwork_Full to output an extra value head. details in the comments

--- policy-network.py
+++ policy-network.py
@@ -19,16 +19,45 @@ class PolicyNetwork_Full(nn.module):
         "Here, we can use modules defined in the constructor (__init__ part defined above) as well as arbitrary operators on Variables"
         h1_relu = F.relu(self.linear1(x))
         h2_relu = F.relu(self.linear2(h1_relu))
-        y_out = F.sigmoid(self.linear3(h2_relu)) #We use sigmoid at the output coz we output probability measures
-        return y_out
+        p_out = F.sigmoid(self.linear3(h2_relu)) #We use sigmoid at the output coz we output probability measures
+        return p_out
 
 class PolicyNetwork_Giraffe(nn.module):
+    def __init__(self, d_in, gf, pc, sc, h1a, h1b, h2c, h2, d_out):
+        "We instantiate various modules"
+
+
+class PolicyValNetwork_Full(nn.module):
+    def __init_(self, d_in, h1, h2p, h2e, d_out, eval_out=1):
+        #h2a are the no.of hidden neurons in the second layer to be used for policy network
+        #h2b are the no.of hidden neurons in the second layer to be used for evaluation network. evaluation network outputs the value function which is a scalar.
+        super(PolicyValNetwork_Full, self).__init__()
+        self.linear1 = nn.Linear(d_in, h1)
+        self.linear2p = nn.Linear(h1, h2p)
+        self.linaer2e = nn.Linear(h1, h2e)
+        self.linear3p = nn.Linear(h2p, d_out)
+        self.linear3e = nn.Linear(h2e, eval_out)
+
+    def forward(self, x):
+        "Here, we will use modules defined in the constructor (__init__ part defined above) as well as arbitray operators on Variables"
+        h1_relu = F.relu(self.linear1(x))
+
+        h2p_relu = F.relu(self.linear2p(h1_relu))
+        p_out = F.sigmoid(self.linear3p(h2p_relu))
+
+        h2e_relu = F.relu(self.linear2e(h1_relu))
+        v_out = self.linear3p(h2e_relu)
+
+        return p_out, v_out
+
 
 
 bs = 1  #batch size
 d_in = 363
 h1 = 1024   #neurons in first hidden layer
 h2 = 2048   #nuerons in second hidden layer
+h2p = 2048  #nuerons in second hidden layer of policy network
+h2e = 512   #neurons in second hidden layer of evaluation network
 d_out = 4096 #without including under promotions. otherwise we have to increase
 
 #splitting the giraffe's feature vector to be input to the network
@@ -42,14 +71,18 @@ piece_centric = 218
 # piece lists with their properties = 2*(1+1+2+2+2+8)*5 = 160
 # sldiing pieces mobility = 2*(8+4+4+4+4) = 48
 # And, I just added extra 10 because, otherwise they are not adding up to 363. Someone pls recheck this.
-sqaure_centric = 128
+square_centric = 128
 ## the following constitute the square-centric features:
 # attack map = 64
 # defend map = 64
+h1a = 32 #no.of first set of neurons in first hidden layer
+h2a = 512 #no.of second set of neurons in second hidden layer
+h3a = 480 #no.of third set of neurons in third hidden layer
 
 # x is your 363-dimensional input. and y is our output. We are randomly initializing them here.
 x = Variable(torch.randn(bs, d_in))
 y = Variable(torch.randn(bs, d_out), requires_grad=False)
 
 model = PolicyNetwork_Full(d_in, h1, h2, d_out)
-model2 = PolicyNetwork_Giraffe(d_in, global_features, piece_centric, square_centric,
+model2 = PolicyNetwork_Giraffe(d_in, global_features, piece_centric, square_centric, h1a, h2a, h3a, h2, d_out)
+model3 = PolicyValNetwork_Full(d_in, h1, h2p, h2e, d_out)
