60bf09f8baf766ac7fde8598c0f5665e80013cbb
sai <saikrishnagottipati@gmail.com>
2018-03-24 18:16:04 -0400

    extracting the exact move

--- try_opening.py
+++ try_opening.py
@@ -4,5 +4,9 @@ import chess.polyglot
 board = chess.Board()
 
 with chess.polyglot.open_reader("./komodo_bin/strong/komodo.bin") as reader:
-    for entry in reader.find_all(board):
-        print(entry.move(), entry.weight, entry.learn)
+    #for entry in reader.find_all(board):
+    print(reader.choice(board).raw_move)
+    for entry in reader.choice(board):
+        #print(entry.move(), entry.weight, entry.learn)
+        print(entry)
+
