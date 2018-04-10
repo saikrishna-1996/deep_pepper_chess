a2eec0417e9d8ee8dfc346901999b8f50f0b4fc8
sai <saikrishnagottipati@gmail.com>
2018-04-10 02:36:16 -0400

    adding self test calls in fullpretrain

--- game/full_pretrain.py
+++ game/full_pretrain.py
@@ -4,6 +4,7 @@ if parentPath not in sys.path:
     sys.path.insert(0,parentPath)
 
 from random import shuffle
+from network.policy_network import PolicyValNetwork_Giraffe
 import chess.pgn
 import chess.uci
 # import random
@@ -17,13 +18,14 @@ import parallel_mcts_test
 
 #set the logger
 logger = Logger('./logs')
+model = PolicyValNetwork_Giraffe
 
 def cross_entropy(pred, soft_targets):
     return torch.mean(torch.sum(- soft_targets.doble() * torch.log(pred).double(), 1))
 
 
 def get_board_position():
-    pgn = open("./game/kasparov.pgn")
+    pgn = open("kasparov.pgn")
     board_positions = []
     try:
         while True:
@@ -55,7 +57,8 @@ def pretrain(model):
             if (index + 1) % Config.minibatch_size != 0:
                 feature_batch.append(board_to_feature(board_position))
                 targets_val_batch.append(stockfish.stockfish_eval(board_position, 10))
-                nvm, targets_pol_batch.append(parallel_mcts_test.value_policy(board_position))
+                nvm, mind = parallel_mcts_test.value_policy(board_position)
+                targets_pol_batch.append(mind)
             else:
                 feature_batch = torch.FloatTensor(feature_batch)
                 targets_val_batch = Variable(torch.FloatTensor(targets_val_batch))
@@ -72,20 +75,20 @@ def do_backprop(batch_features, targets_val, targets_pol, model, iters):
     criterion1 = torch.nn.MSELoss(size_average=False)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     nn_policy_out, nn_val_out = model(batch_features)
-    
+
     loss1 = criterion1(nn_val_out, targets_val)
     loss2 = cross_entropy(nn_policy_out, targets_pol)
-    
+
     l2_reg = None
     for weight in model.parameters():
-        if l2.reg is None:
+        if l2_reg is None:
             l2_reg = weight.norm(2)
         else:
             l2_reg = l2_reg + weight.norm(2)
     loss3 = 0.1 * l2_reg
 
     loss = loss1.float() - loss2.float() + loss3.float()
-    
+    print(iters)
     info = {
             'full_pt_loss1': loss1.data[0],
             'full_pt_loss2': loss2.data[0],
@@ -99,3 +102,5 @@ def do_backprop(batch_features, targets_val, targets_pol, model, iters):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+pretrain(model)
