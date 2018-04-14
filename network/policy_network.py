1735f3e092005532b65c0c5cccb10539a264bfa4
sai <saikrishnagottipati@gmail.com>
2018-04-14 15:11:32 -0400

    finsihed policynetwork_giraffe

--- network/policy_network.py
+++ network/policy_network.py
@@ -27,31 +27,47 @@ class PolicyNetwork_Full(nn.Module):
 
 
 class PolicyNetwork_Giraffe(nn.Module):
-    def __init__(self, d_in, gf, pc, sc, h1a, h1b, h1c, h2, d_out):
+    def __init__(self,
+            pretrain=False,
+            d_in = Config.d_in,
+            gf = Config.global_features,
+            pc = Config.piece_centric,
+            sc = Config.square_centric,
+            h1a = Config.h1a,
+            h1b = Config.h1b,
+            h1c = Config.h1c,
+            h2 = Config.h2,
+            d_out = Config.d_out,
+            eval_out = 1):
         "We instantiate various modules"
         super(PolicyNetwork_Giraffe, self).__init__()
+        self.gf = gf
+        self.pc = pc
+        self.sc = sc
         self.linear1a = nn.Linear(gf, h1a)
         self.linear1b = nn.Linear(pc, h1b)
         self.linear1c = nn.Linear(sc, h1c)
         self.linear2 = nn.Linear(h1a + h1b + h1c, h2)
         self.linear3 = nn.Linear(h2, d_out)
+        if pretrain:
+            pretrain(self)
 
     def forward(self, x):
-        x = Variable(x)
+        x = Variable(x.float())
         "Here, we can use modules defined in the constrcutor (__init__ part defined above) as well as arbitrary operators on Variables"
         gf = self.gf
         pc = self.pc
         sc = self.sc
 
-        x1 = x[:, 0:gf - 1]
-        x2 = x[:, gf:gf + pc - 1]
-        x3 = x[:, gf + pc:gf + pc + sc - 1]
+        x1 = x[:, 0:gf]
+        x2 = x[:, gf:gf + pc]
+        x3 = x[:, gf + pc:gf + pc + sc]
         h1a_relu = F.relu(self.linear1a(x1))
         h1b_relu = F.relu(self.linear1b(x2))
         h1c_relu = F.relu(self.linear1c(x3))
-        h1_relu = torch.cat(h1a_relu, h1b_relu, h1c_relu, dim=1)
+        h1_relu = torch.cat((h1a_relu, h1b_relu, h1c_relu), dim=1)
         h2_relu = F.relu(self.linear2(h1_relu))
-        p_out = F.sigmoid(self.linear3(h2_relu))
+        p_out = F.log_softmax(self.linear3(h2_relu))
 
         return p_out
 
