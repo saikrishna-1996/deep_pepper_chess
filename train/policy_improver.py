166f25840e5031c15da02230ee8f452c86e4ce79
sai <saikrishnagottipati@gmail.com>
2018-04-14 22:55:02 -0400

    added value stuff in policy_improver

--- train/policy_improver.py
+++ train/policy_improver.py
@@ -10,11 +10,12 @@ class PolicyImprover(object):
         self.championship_rounds = championship_rounds
 
     def train_model(self, new_games):
-        model = copy.copy(self.champion.current_policy)
-        return train_model(model=model, games=new_games, min_num_games=self.championship_rounds)
+        pol_model = copy.copy(self.champion.current_policy)
+        val_model = copy.copy(self.champion.current_value)
+        return train_model(pol_model = pol_model, val_model = val_model, games=new_games, min_num_games=self.championship_rounds)
 
     def improve_policy(self, games, pool):
         start = time.time()
-        new_policy = self.train_model(games)
-        self.champion.test_candidate(new_policy, pool)
+        new_policy, new_value = self.train_model(games)
+        self.champion.test_candidate(new_policy, new_value, pool)
         print("Improved policy in: {}".format(time.time() - start))
