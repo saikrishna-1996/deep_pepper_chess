62f9cbfa8863bcfa63d07332db39299c27017bad
sai <saikrishnagottipati@gmail.com>
2018-04-25 00:56:01 -0400

    changes to label_script. verify the change

--- game/label_script.sh
+++ game/label_script.sh
@@ -1,10 +1,10 @@
 
-python3 pgn2boards.py --player-file 'Karpov.pgn'  &&
+python3 pgn2boards.py --player-file 'miniature.pgn' &&
 
 for index in $(seq 0 10 1000000);#
 do
-python3 board_labeling.py --board-index $index --dump-path '/media/ahmad/ahmad/topalov_files/' --player-file 'Karpov.pgn'
+python3 board_labeling.py --board-index $index --dump-path '/u/gottipav/deep_pepper_chess/game/' --player-file 'miniature.pgn'
 echo $index
 done
 &&
-python3 joining_boards.py --load-path '/media/ahmad/ahmad/Karpov_files/'
+python3 joining_boards.py --load-path '/u/gottipav/deep_pepper_chess/game/'
