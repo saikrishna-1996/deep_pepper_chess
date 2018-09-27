9a7c39ddf637d2995a044b0d355786d5a2c5033e
saikrishna-1996 <saikrishnagv1996@gmail.com>
2018-09-27 19:39:03 -0400

    pretraining value net

--- game/pretrain_value_net.py
+++ game/pretrain_value_net.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from networks import perceptual_conv_fc 
+from networks import Critic_Giraffe 
 from logger import Logger
 from datetime import datetime
 import argparse
@@ -21,7 +21,7 @@ class my_trainer(object):
     def __init__(self):
         self.num_epochs = 100
         self.batch_size = 32
-        self.value_model = perceptual_conv_fc(11)
+        self.value_model = Critic_Giraffe()
 
         if args.gpu == True and torch.cuda.is_available():
             self.device = torch.device("cuda")
@@ -32,47 +32,53 @@ class my_trainer(object):
 
         if self.device == torch.device("cuda") and torch.cuda.device_count() > 1:
             print ("Use", torch.cuda.device_count(), 'GPUs')
-            self.perceptual_model = nn.DataParallel(self.perceptual_model)
+            self.value_model = nn.DataParallel(self.value_model)
         else:
             print ("Use CPU")
 
 
-        self.perceptual_model.to(self.device)
+        self.value_model.to(self.device)
 
-        self.optimizer = torch.optim.Adam(list(self.perceptual_model.parameters()), lr=args.lrpm)
+        self.optimizer = torch.optim.Adam(list(self.value_model.parameters()), lr=args.lrpm)
         self.criterion = nn.MSELoss() 
-        self.input_stuff = np.load('/home/sai/data_keehong_20180924/lm_in.npy')
-        self.gt_output_stuff = np.load('/home/sai/data_keehong_20180924/lm_out.npy')
+        self.input_stuff = np.load('/home/sai/deep_pepper_chess/game/features.txt.npy')
+        self.gt_output_stuff = np.load('/home/sai/deep_pepper_chess/game/values.txt.npy')
+        sizee = self.gt_output_stuff.shape[0]
+        # np.resize(a,(2,3))
+        np.resize(self.gt_output_stuff, (sizee,1))
+        print(self.gt_output_stuff.shape)
+        self.gt_output_stuff = np.expand_dims(self.gt_output_stuff, axis=1)
         self.back_prop()
 
     def do_train(self, input_batch, target_batch, i):
-        self.perceptual_model.train()
+        self.value_model.train()
         self.optimizer.zero_grad()
-        output = self.perceptual_model(input_batch)
+        output = self.value_model(input_batch)
         loss = self.criterion(output, target_batch)
         loss.backward()
         self.optimizer.step()
         info = {'likelihood_loss': loss.item()}
         for tag, value in info.items():
             logger.scalar_summary(tag, value, i)
-        # for tag, value in self.perceptual_model.named_parameters():
+        # for tag, value in self.value_model.named_parameters():
         #     tag = tag.replace('.', '/')
         #     logger.histo_summary(tag, value.data.cpu().numpy(), i)
         #     logger.histo_summary(tag+'/perceptual_grad', value.grad.data.cpu().numpy(), i)
 
     def do_test(self, input_batch, target_batch, i):
-        self.perceptual_model.eval()
-        output = self.perceptual_model(input_batch)
+        self.value_model.eval()
+        output = self.value_model(input_batch)
         loss = self.criterion(output, target_batch)
         print("test loss is ", loss)
 
     def back_prop(self):
 
         my_size = self.input_stuff.shape[0]
-        num_batches = my_size / self.batch_size 
+        num_batches = int(my_size / self.batch_size) 
         print("total samples = ", my_size)
         print("batch size = ", self.batch_size)
         print("num_batches = ", num_batches)
+        print(self.gt_output_stuff.shape)
         for j in range(self.num_epochs):
             for i in range(num_batches):
                 print(j * num_batches + i)
@@ -86,14 +92,15 @@ class my_trainer(object):
 
                 self.do_train(input_batch, target_batch, j * num_batches + i)
 
+            home = '/home/sai/deep_pepper_chess/'
             str_date_time = datetime.now().strftime('%Y%m%d-%H%M%S')
-            self.logfile = 'anl_output-%s.txt'%str_date_time
-            modelfile='perceptual_models/'+'model_'+self.logfile
-            self.vn_filepath=os.path.join(home,anl_loc,modelfile)
-            torch.save(self.perceptual_model.state_dict(), self.pm_filepath)
+            self.logfile = 'val_output-%s.txt'%str_date_time
+            modelfile='value_models/'+'model_'+self.logfile
+            self.vn_filepath=os.path.join(home,modelfile)
+            torch.save(self.value_model.state_dict(), self.vn_filepath)
 
         print("backprop done")
-        torch.save(self.perceptual_model.state_dict(), self.pm_filepath)
-        print ('value network model saved at %s.'%self.pm_filepath)
+        torch.save(self.value_model.state_dict(), self.vn_filepath)
+        print ('value network model saved at %s.'%self.vn_filepath)
 
 my_trainer()
